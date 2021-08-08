912089df1d44dd4265dc44e2e2fe2bd801012b2a
Aji <prabowo.aji@gmail.com>
2021-08-08 21:56:30 +0700

    Need code review on how to get values from marshalled json

--- test/integration/server_test.go
+++ test/integration/server_test.go
@@ -1,6 +1,9 @@
 package integration
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,7 +13,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+/*********************************
+	{
+		"num_of_workers":0,
+		"state":"NotStarted"
+	}
+**********************************/
+type ServerStruct struct {
+	Num_of_workers int
+	State          string
+}
+
 func TestHandleServerInfo(t *testing.T) {
+
+	assert := assert.New(t)
 
 	//Mock server
 	server := server.NewServer()
@@ -25,9 +41,41 @@ func TestHandleServerInfo(t *testing.T) {
 
 	resp := w.Result()
 
-	assert.Equal(t, resp.StatusCode, http.StatusOK)
+	if resp.StatusCode == http.StatusOK {
+		var ServerInfo ServerStruct
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bodyString := string(bodyBytes)
+		json.Unmarshal([]byte(bodyString), &ServerInfo)
+		log.Printf(bodyString)
+		log.Printf("Num of Workers: %d, States: %s", ServerInfo.Num_of_workers, ServerInfo.State)
+
+		var expectedNumOfWorkers = 0
+		var expectedState = "NotStarted"
+		assert.Equal(expectedNumOfWorkers, ServerInfo.Num_of_workers, "The two number of workers should be the same")
+		assert.Equal(expectedState, ServerInfo.State, "The two states should be the same")
+	} else {
+		assert.Equal(resp.StatusCode, http.StatusOK)
+	}
+
 }
+
+/*********************************
+	{
+		"name":"worker1",
+		"state":""
+	}
+**********************************/
+type WorkersStruct struct {
+	name  string
+	State string
+}
+
 func TestHandleWorkersInfo(t *testing.T) {
+
+	assert := assert.New(t)
 
 	//Mock server
 	server := server.NewServer()
@@ -43,6 +91,7 @@ func TestHandleWorkersInfo(t *testing.T) {
 		connected <- struct{}{}
 	})
 
+	worker.SetName("worker1")
 	go worker.Run("127.0.0.1:9019")
 	<-connected
 
@@ -54,5 +103,24 @@ func TestHandleWorkersInfo(t *testing.T) {
 
 	resp := w.Result()
 
-	assert.Equal(t, resp.StatusCode, http.StatusOK)
+	//assert.Equal(t, resp.StatusCode, http.StatusOK)
+
+	if resp.StatusCode == http.StatusOK {
+		var WorkerInfo WorkersStruct
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bodyString := string(bodyBytes)
+		json.Unmarshal([]byte(bodyString), &WorkerInfo)
+
+		log.Printf(bodyString)
+		log.Printf("Name: %s", WorkerInfo.name)
+
+		var expectedName = "worker1"
+		assert.Equal(expectedName, WorkerInfo.name, "The two number of workers should be the same")
+
+	} else {
+		assert.Equal(resp.StatusCode, http.StatusOK)
+	}
 }
