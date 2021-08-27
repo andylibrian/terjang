6e6f23504869504581f1df94bf9784337d5013a0
ajiprabowo <prabowo.aji@gmail.com>
2021-08-28 03:55:19 +0700

    test(API): verify response body (#16)

--- test/integration/server_test.go
+++ test/integration/server_test.go
@@ -1,18 +1,22 @@
 package integration
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/andylibrian/terjang/pkg/messages"
 	"github.com/andylibrian/terjang/pkg/server"
 	"github.com/andylibrian/terjang/pkg/worker"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestHandleServerInfo(t *testing.T) {
 
-	//Mock server
 	server := server.NewServer()
 	go server.Run("127.0.0.1:9029")
 	defer server.Close()
@@ -25,11 +29,31 @@ func TestHandleServerInfo(t *testing.T) {
 
 	resp := w.Result()
 
-	assert.Equal(t, resp.StatusCode, http.StatusOK)
+	require.Equal(t, resp.StatusCode, http.StatusOK)
+
+	var ServerResult messages.ServerInfo
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	json.Unmarshal(bodyBytes, &ServerResult)
+	log.Printf("%s", string(bodyBytes))
+	log.Printf("Num of Workers: %d, States: %s", ServerResult.NumOfWorkers, ServerResult.State)
+
+	assert.Equal(t, 0, ServerResult.NumOfWorkers)
+	assert.Equal(t, "NotStarted", ServerResult.State)
+
 }
+
+type WorkerStruct struct {
+	Name    string                         `json:"name"`
+	Metrics messages.WorkerLoadTestMetrics `json:"metrics"`
+	State   string                         `json:"state"`
+}
+
 func TestHandleWorkersInfo(t *testing.T) {
 
-	//Mock server
 	server := server.NewServer()
 	go server.Run("127.0.0.1:9019")
 	defer server.Close()
@@ -43,6 +67,7 @@ func TestHandleWorkersInfo(t *testing.T) {
 		connected <- struct{}{}
 	})
 
+	worker.SetName("worker1")
 	go worker.Run("127.0.0.1:9019")
 	<-connected
 
@@ -54,5 +79,20 @@ func TestHandleWorkersInfo(t *testing.T) {
 
 	resp := w.Result()
 
-	assert.Equal(t, resp.StatusCode, http.StatusOK)
+	require.Equal(t, resp.StatusCode, http.StatusOK)
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var result []WorkerStruct
+
+	err = json.Unmarshal(bodyBytes, &result)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "worker1", result[0].Name)
+
 }
